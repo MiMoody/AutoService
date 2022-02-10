58ab7c5bd9925161bc681820baef258854d01a5e
Artem <svev2369@gmail.com>
2022-02-10 20:33:29 +0700

    Add sort and base function

---
+++ Autoserv/MainForm.Designer.cs
@@ -0,0 +1,248 @@
+﻿
+namespace Autoserv
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.BottomPanel = new System.Windows.Forms.Panel();
+            this.MainText = new System.Windows.Forms.Label();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.ClientTable = new System.Windows.Forms.DataGridView();
+            this.BtnPreviosPage = new System.Windows.Forms.Button();
+            this.BtnNextPage = new System.Windows.Forms.Button();
+            this.PageLab = new System.Windows.Forms.Label();
+            this.CmbPagesCount = new System.Windows.Forms.ComboBox();
+            this.CmbGenderFilter = new System.Windows.Forms.ComboBox();
+            this.PageFilterLab = new System.Windows.Forms.Label();
+            this.GenderFilterLab = new System.Windows.Forms.Label();
+            this.MainPanel.SuspendLayout();
+            this.TopPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ClientTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.MainPanel.Controls.Add(this.GenderFilterLab);
+            this.MainPanel.Controls.Add(this.PageFilterLab);
+            this.MainPanel.Controls.Add(this.CmbGenderFilter);
+            this.MainPanel.Controls.Add(this.CmbPagesCount);
+            this.MainPanel.Controls.Add(this.PageLab);
+            this.MainPanel.Controls.Add(this.BtnNextPage);
+            this.MainPanel.Controls.Add(this.BtnPreviosPage);
+            this.MainPanel.Controls.Add(this.ClientTable);
+            this.MainPanel.Controls.Add(this.BottomPanel);
+            this.MainPanel.Controls.Add(this.TopPanel);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MainPanel.Location = new System.Drawing.Point(0, 0);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(1088, 471);
+            this.MainPanel.TabIndex = 0;
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(225)))));
+            this.TopPanel.Controls.Add(this.BtnExit);
+            this.TopPanel.Controls.Add(this.MainText);
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(1088, 54);
+            this.TopPanel.TabIndex = 0;
+            // 
+            // BottomPanel
+            // 
+            this.BottomPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(225)))));
+            this.BottomPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BottomPanel.Location = new System.Drawing.Point(0, 421);
+            this.BottomPanel.Name = "BottomPanel";
+            this.BottomPanel.Size = new System.Drawing.Size(1088, 50);
+            this.BottomPanel.TabIndex = 1;
+            // 
+            // MainText
+            // 
+            this.MainText.AutoSize = true;
+            this.MainText.Location = new System.Drawing.Point(12, 16);
+            this.MainText.Name = "MainText";
+            this.MainText.Size = new System.Drawing.Size(155, 22);
+            this.MainText.TabIndex = 0;
+            this.MainText.Text = "Список клиентов";
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnExit.Location = new System.Drawing.Point(1043, 12);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(33, 32);
+            this.BtnExit.TabIndex = 1;
+            this.BtnExit.Text = "X";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // ClientTable
+            // 
+            this.ClientTable.AllowUserToAddRows = false;
+            this.ClientTable.AllowUserToDeleteRows = false;
+            this.ClientTable.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ClientTable.GridColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientTable.Location = new System.Drawing.Point(16, 98);
+            this.ClientTable.Name = "ClientTable";
+            this.ClientTable.ReadOnly = true;
+            this.ClientTable.Size = new System.Drawing.Size(1050, 269);
+            this.ClientTable.TabIndex = 2;
+            // 
+            // BtnPreviosPage
+            // 
+            this.BtnPreviosPage.Location = new System.Drawing.Point(16, 388);
+            this.BtnPreviosPage.Name = "BtnPreviosPage";
+            this.BtnPreviosPage.Size = new System.Drawing.Size(53, 27);
+            this.BtnPreviosPage.TabIndex = 3;
+            this.BtnPreviosPage.Text = "<<";
+            this.BtnPreviosPage.UseVisualStyleBackColor = true;
+            this.BtnPreviosPage.Click += new System.EventHandler(this.BtnPreviosPage_Click);
+            // 
+            // BtnNextPage
+            // 
+            this.BtnNextPage.Location = new System.Drawing.Point(75, 388);
+            this.BtnNextPage.Name = "BtnNextPage";
+            this.BtnNextPage.Size = new System.Drawing.Size(53, 27);
+            this.BtnNextPage.TabIndex = 4;
+            this.BtnNextPage.Text = ">>";
+            this.BtnNextPage.UseVisualStyleBackColor = true;
+            this.BtnNextPage.Click += new System.EventHandler(this.BtnNextPage_Click);
+            // 
+            // PageLab
+            // 
+            this.PageLab.AutoSize = true;
+            this.PageLab.Location = new System.Drawing.Point(146, 391);
+            this.PageLab.Name = "PageLab";
+            this.PageLab.Size = new System.Drawing.Size(32, 22);
+            this.PageLab.TabIndex = 5;
+            this.PageLab.Text = "10";
+            // 
+            // CmbPagesCount
+            // 
+            this.CmbPagesCount.AutoCompleteCustomSource.AddRange(new string[] {
+            "10",
+            "50",
+            "200",
+            "Все"});
+            this.CmbPagesCount.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CmbPagesCount.FormattingEnabled = true;
+            this.CmbPagesCount.Items.AddRange(new object[] {
+            "Все",
+            "10",
+            "30",
+            "200"});
+            this.CmbPagesCount.Location = new System.Drawing.Point(906, 61);
+            this.CmbPagesCount.Margin = new System.Windows.Forms.Padding(4);
+            this.CmbPagesCount.Name = "CmbPagesCount";
+            this.CmbPagesCount.Size = new System.Drawing.Size(160, 30);
+            this.CmbPagesCount.TabIndex = 11;
+            this.CmbPagesCount.SelectedIndexChanged += new System.EventHandler(this.CmbPagesCount_SelectedIndexChanged);
+            // 
+            // CmbGenderFilter
+            // 
+            this.CmbGenderFilter.AutoCompleteCustomSource.AddRange(new string[] {
+            "Все",
+            "М",
+            "Ж"});
+            this.CmbGenderFilter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CmbGenderFilter.FormattingEnabled = true;
+            this.CmbGenderFilter.Items.AddRange(new object[] {
+            "Все",
+            "М",
+            "Ж"});
+            this.CmbGenderFilter.Location = new System.Drawing.Point(545, 61);
+            this.CmbGenderFilter.Margin = new System.Windows.Forms.Padding(4);
+            this.CmbGenderFilter.Name = "CmbGenderFilter";
+            this.CmbGenderFilter.Size = new System.Drawing.Size(160, 30);
+            this.CmbGenderFilter.TabIndex = 12;
+            this.CmbGenderFilter.SelectedIndexChanged += new System.EventHandler(this.CmbGenderFilter_SelectedIndexChanged);
+            // 
+            // PageFilterLab
+            // 
+            this.PageFilterLab.AutoSize = true;
+            this.PageFilterLab.Location = new System.Drawing.Point(712, 64);
+            this.PageFilterLab.Name = "PageFilterLab";
+            this.PageFilterLab.Size = new System.Drawing.Size(192, 22);
+            this.PageFilterLab.TabIndex = 13;
+            this.PageFilterLab.Text = "Количество страниц:";
+            // 
+            // GenderFilterLab
+            // 
+            this.GenderFilterLab.AutoSize = true;
+            this.GenderFilterLab.Location = new System.Drawing.Point(385, 64);
+            this.GenderFilterLab.Name = "GenderFilterLab";
+            this.GenderFilterLab.Size = new System.Drawing.Size(158, 22);
+            this.GenderFilterLab.TabIndex = 14;
+            this.GenderFilterLab.Text = "Фильтер по полу:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1088, 471);
+            this.ControlBox = false;
+            this.Controls.Add(this.MainPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Главное меню";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
+            this.TopPanel.ResumeLayout(false);
+            this.TopPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ClientTable)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.Panel TopPanel;
+        private System.Windows.Forms.Panel BottomPanel;
+        private System.Windows.Forms.Label MainText;
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.DataGridView ClientTable;
+        private System.Windows.Forms.Label PageLab;
+        private System.Windows.Forms.Button BtnNextPage;
+        private System.Windows.Forms.Button BtnPreviosPage;
+        private System.Windows.Forms.ComboBox CmbPagesCount;
+        private System.Windows.Forms.Label GenderFilterLab;
+        private System.Windows.Forms.Label PageFilterLab;
+        private System.Windows.Forms.ComboBox CmbGenderFilter;
+    }
+}
+
---
+++ Autoserv/MainForm.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Autoserv
+{
+    public partial class MainForm : Form
+    {
+        int TotalPages;
+        int CurrentIndexPage = 1;
+        int PageSize;
+        AutoservEntities db = new AutoservEntities();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateTotalPages()
+        {
+            int RowCount = 0;
+            if (CmbGenderFilter.SelectedIndex == 0)
+                RowCount = Convert.ToInt32(db.Client.ToList().Count());
+            else if (CmbGenderFilter.SelectedIndex == 1)
+                RowCount = Convert.ToInt32(db.Client.ToList().Where(p => p.GenderCode == "м").Count());
+            else if (CmbGenderFilter.SelectedIndex == 2)
+                RowCount = Convert.ToInt32(db.Client.ToList().Where(p => p.GenderCode == "ж").Count());
+
+            TotalPages = RowCount / PageSize;
+            if (RowCount % PageSize > 0)
+                TotalPages += 1;
+        }
+
+        public string PrintLabText(bool check, int page = 1)
+        {
+            int PreviousPageOffSet = (page - 1) * PageSize;
+            if (check)
+            {
+                
+                if (CmbGenderFilter.SelectedIndex == 0)
+                {
+                    var before = db.Client.Select(p => p.ID).Take(PreviousPageOffSet).Count();
+                    var NoProviders = db.Client.Select(p => p.ID).Take(PreviousPageOffSet).ToArray();
+                    var after = db.Client.Where(p => !NoProviders.Contains(p.ID)).Take(PageSize).Count();
+                    return (before + after).ToString() + " из " + db.Client.Count().ToString();
+                }
+                else if (CmbGenderFilter.SelectedIndex == 1)
+                {
+                    var before = db.Client.Where(p => p.GenderCode == "м").Select(p => p.ID).Take(PreviousPageOffSet).Count();
+                    var NoProviders = db.Client.Where(p => p.GenderCode == "м").Select(p => p.ID).Take(PreviousPageOffSet).ToArray();
+                    var after = db.Client.Where(p => !NoProviders.Contains(p.ID) && p.GenderCode =="м").Take(PageSize).Count();
+                    return (before + after).ToString() + " из " + db.Client.Where(p => p.GenderCode == "м").Count().ToString();
+                }
+
+                else if (CmbGenderFilter.SelectedIndex == 2)
+                {
+                    var before = db.Client.Where(p => p.GenderCode == "ж").Select(p => p.ID).Take(PreviousPageOffSet).Count();
+                    var NoProviders = db.Client.Where(p => p.GenderCode == "ж").Select(p => p.ID).Take(PreviousPageOffSet).ToArray();
+                    var after = db.Client.Where(p => !NoProviders.Contains(p.ID) && p.GenderCode == "ж").Take(PageSize).Count();
+                    return (before + after).ToString() + " из " + db.Client.Where(p => p.GenderCode == "ж").Count().ToString();
+                }
+
+                return "";
+            }
+            else
+            {
+                if (CmbGenderFilter.SelectedIndex == 0)
+                    return db.Client.OrderBy(p => p.ID).Take(PageSize).Count().ToString() + " из " + db.Client.Count().ToString();
+                else if (CmbGenderFilter.SelectedIndex == 1)
+                    return db.Client.OrderBy(p => p.ID).Where(p => p.GenderCode == "м").Take(PageSize).Count().ToString() + " из " + db.Client.Where(p => p.GenderCode == "м").Count().ToString();
+                else if (CmbGenderFilter.SelectedIndex == 2)
+                    return db.Client.OrderBy(p => p.ID).Where(p => p.GenderCode == "ж").Take(PageSize).Count().ToString() + " из " + db.Client.Where(p => p.GenderCode == "ж").Count().ToString();
+                return "";
+            }
+
+                
+        }
+        private List<Client> GetCurrentRecords(int page)
+        {
+            List<Client> list = new List<Client>();
+
+            if (page == 1)
+            {
+                if (CmbGenderFilter.SelectedIndex == 0)
+                    list = db.Client.OrderBy(p => p.ID).Take(PageSize).ToList();
+                else if (CmbGenderFilter.SelectedIndex == 1)
+                    list = db.Client.OrderBy(p => p.ID).Where(p => p.GenderCode == "м").Take(PageSize).ToList();
+                else if (CmbGenderFilter.SelectedIndex == 2)
+                    list = db.Client.OrderBy(p => p.ID).Where(p => p.GenderCode == "ж").Take(PageSize).ToList();
+                PageLab.Text = PrintLabText(false, page);
+            }
+            else
+            {
+                int PreviousPageOffSet = (page - 1) * PageSize;
+                int[] NoProviders = null;
+                if (CmbGenderFilter.SelectedIndex == 0)
+                {
+                    NoProviders = db.Client.Select(p => p.ID).Take(PreviousPageOffSet).ToArray();
+                    list = db.Client.Where(p => !NoProviders.Contains(p.ID)).Take(PageSize).ToList();
+                }
+                else if (CmbGenderFilter.SelectedIndex == 1)
+                {
+                    NoProviders = db.Client.Where(p => p.GenderCode == "м").Select(p => p.ID).Take(PreviousPageOffSet).ToArray();
+                    list = db.Client.Where(p => !NoProviders.Contains(p.ID) && p.GenderCode == "м").Take(PageSize).ToList();
+                }
+                    
+                else if (CmbGenderFilter.SelectedIndex == 2)
+                {
+                    NoProviders = db.Client.Where(p => p.GenderCode == "ж").Select(p => p.ID).Take(PreviousPageOffSet).ToArray();
+                    list = db.Client.Where(p => !NoProviders.Contains(p.ID) && p.GenderCode == "ж").Take(PageSize).ToList();
+                }
+                   
+                PageLab.Text = PrintLabText(true, page);
+            }
+
+            return list;
+        }
+
+        public void UpdateTable()
+        {
+            if (CmbPagesCount.SelectedIndex == -1)
+            CmbPagesCount.SelectedIndex = 0;
+            if (CmbGenderFilter.SelectedIndex == -1)
+            CmbGenderFilter.SelectedIndex = 0;
+            List<Client> clients = new List<Client>();
+            string check = null;
+            if (CmbGenderFilter.SelectedIndex == 0) check = "Все";
+            if (CmbGenderFilter.SelectedIndex == 1) check = "м";
+            if (CmbGenderFilter.SelectedIndex == 2) check = "ж";
+            foreach (Client client in db.Client.ToList())
+            {
+                if (client.GenderCode != check && check != "Все") continue;
+                client.Quantity = db.ClientService.Where(p => p.ClientID == client.ID).Count();
+                DateTime LastTime = DateTime.MinValue;
+                foreach (ClientService clientService in db.ClientService.ToList())
+                    if (clientService.StartTime > LastTime) LastTime = clientService.StartTime;
+                client.LastDate = LastTime;
+                foreach (TagOfClient tagOfClient in db.TagOfClient.ToList())
+                    if (client.ID == tagOfClient.ClientID) client.Tags += tagOfClient.Tag.Title + ";";
+                clients.Add(client);
+            }
+            PageSize = Convert.ToInt32(clients.Count());
+            CalculateTotalPages();
+            PageLab.Text = PrintLabText(false,1);
+            ClientTable.DataSource = clients;
+            ClientTable.Columns[9].Visible = false;
+            ClientTable.Columns[13].Visible = false;
+            ClientTable.Columns[14].Visible = false;
+            ClientTable.Columns[15].Visible = false;
+            ClientTable.Columns[0].HeaderText = "ID";
+            ClientTable.Columns[1].HeaderText = "Имя";
+            ClientTable.Columns[2].HeaderText = "Фамилия";
+            ClientTable.Columns[3].HeaderText = "Отчество";
+            ClientTable.Columns[4].HeaderText = "Дата рождения";
+            ClientTable.Columns[5].HeaderText = "Дата добавления";
+            ClientTable.Columns[6].HeaderText = "Email";
+            ClientTable.Columns[7].HeaderText = "Телефон";
+            ClientTable.Columns[8].HeaderText = "Пол";
+            ClientTable.Columns[10].HeaderText = "Последнее посещение";
+            ClientTable.Columns[11].HeaderText = "Количество посещений";
+            ClientTable.Columns[12].HeaderText = "Список тегов";
+        }
+
+       
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            UpdateTable();
+        }
+
+        private void CmbPagesCount_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (CmbPagesCount.SelectedIndex == 0) UpdateTable();
+            else
+            {
+                PageSize = Convert.ToInt32(CmbPagesCount.SelectedItem);
+                CalculateTotalPages();
+                ClientTable.DataSource = GetCurrentRecords(1);
+
+            }
+        }
+
+        private void BtnNextPage_Click(object sender, EventArgs e)
+        {
+            if (CurrentIndexPage < TotalPages)
+            {
+                CurrentIndexPage++;
+                ClientTable.DataSource =
+                GetCurrentRecords(CurrentIndexPage);
+            }
+        }
+
+        private void BtnPreviosPage_Click(object sender, EventArgs e)
+        {
+            if (CurrentIndexPage > 1)
+            {
+                CurrentIndexPage--;
+                ClientTable.DataSource =
+                GetCurrentRecords(CurrentIndexPage);
+            }
+        }
+
+        private void CmbGenderFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (CmbGenderFilter.SelectedIndex == 0 || CmbPagesCount.SelectedIndex == 0 ) UpdateTable();
+            else
+            {
+                PageSize = Convert.ToInt32(CmbPagesCount.SelectedItem);
+                CalculateTotalPages();
+                ClientTable.DataSource = GetCurrentRecords(1);
+            }
+        }
+    }
+}
